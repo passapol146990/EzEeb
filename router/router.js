227a8bcc12f7da6d600ff1deb6981efe9cfcb7e6
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-07-16 07:24:01 +0700

    1.2

--- router/router.js
+++ router/router.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const admin = require('firebase-admin');
 const serviceAccount = require('../mywebez-3f338-firebase-adminsdk-warup-6727451858.json');
 
+let datapass = {}
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -23,6 +24,19 @@ router.get('/',async (req,res)=>{
   view = views.docs.map(view => view.data())
   sessions.views = view[0].views + 1
   await db.collection('views').add({views:sessions.views})
+  if(req.cookies.login){
+    sessions.data = data
+    sessions.name = datapass[req.cookies.username].name
+    sessions.username = req.cookies.username
+    sessions.reg_show = ''
+    sessions.inout = '<a class="btn btn-outline-danger" href="/logout">ออกจากระบบ</a>'
+  }else{
+    sessions.name = ''
+    sessions.username = ''
+    sessions.data = ''
+    sessions.reg_show = '<p>ทำตามเงื่อนไขหากยังไม่มีบัญชี <a href="/regform">สร้างบัญชี</a></p>'
+    sessions.inout = '<a class="btn btn-outline-light" href="/loginform">เข้าสู่ระบบ</a>'
+  }
   res.render('index.ejs',{sessions:sessions})
 })
 
